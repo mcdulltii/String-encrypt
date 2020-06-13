110ad4d05342593e64b56006936846d6114b8c17
root <root@localhost.localdomain>
2020-06-13 05:00:07 -0400

    Second draft

--- parser.py
+++ parser.py
@@ -13,7 +13,7 @@ parser.add_argument('--debug', '-d', help='Enable debug mode', default=False, ac
 args = parser.parse_args()
 input_str = args.str
 length = len(input_str)
-encoding = args.encode
+encoding = int(args.encode)
 debugging = args.debug
 output_file = args.output
 
@@ -43,33 +43,43 @@ def sub(input_string, rand_int):
     return input_string - rand_int
 
 
+def index(input_string, index, rand_int, encode_func, rand_seq_int):
+    return encode_func[rand_int](input_string, rand_seq_int)
+
+
 def encode(input_list, encode_func):
-    encode_seq = [random.randint(0,3) for i in range(encoding)]
-    rand_seq = [random.randint(1,255) for i in range(encoding)]
+    temp_seq = [random.randint(0,len(encode_func)-1) for i in range(encoding)]
+    encode_seq = [[i,random.randint(0,len(encode_func)-2)] if i==4 else i for i in temp_seq]
+    rand_seq = [random.randint(1,255) for i in range(len(encode_seq))]
 
     output_seq = []
     for i in range(length):
         temp = input_list[i]
         for j in range(encoding):
-            temp = encode_func[encode_seq[j]](temp, rand_seq[j])
+            if isinstance(encode_seq[j], list):
+                temp = encode_func[encode_seq[j][0]](temp, i, encode_seq[j][1], encode_func, rand_seq[j])
+            else:
+                temp = encode_func[encode_seq[j]](temp, rand_seq[j])
         output_seq.append(temp)
     return encode_seq, rand_seq, output_seq
 
 
 def main():
     encode_fmt = ['^=', '= ~', '+=', '-=']
-    encode_names = ["XOR", "NEG", "ADD", "SUB"]
-    encode_func = [xor, neg, add, sub]
-    header = "#include <stdlib.h>\n#include <stdio.h>\n\nunsigned char str[%s] = {" % (length)
+    encode_func = [xor, neg, add, sub, index]
+    header = "#include <stdlib.h>\n#include <stdio.h>\n\nunsigned char str[%s] = { " % (length)
     input_list = [ord(i) for i in input_str]
     debug("Input list", input_list)
 
     encode_seq, rand_seq, output_seq = encode(input_list, encode_func)
+    while (sum([False if 0<=i<256 else True for i in output_seq]) > 0):
+        encode_seq, rand_seq, output_seq = encode(input_list, encode_func)
     debug("Encoding index", encode_seq)
-    debug("Encoding sequence", [encode_names[i] for i in encode_seq])
     debug("Random integers", rand_seq)
     debug("Encoded inputs", output_seq)
+    header += ", ".join([hex(i) for i in output_seq]) + " };\n\n"
 
+    body = "int main() {\nfor(unsigned int % = 0, % = 0; )"
     return
 
 
